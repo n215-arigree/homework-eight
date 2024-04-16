cd5bfe64706c989b4a487e828e30042184d95671
arigree <arigree@iu.edu>
2024-04-16 06:16:09 -0400

    testing why not working on server

--- app/app.js
+++ app/app.js
@@ -1,27 +1,25 @@
+var homeContent = ``;
+var aboutContent = ``;
+var productsContent = ``;
+var contactContent = ``;
+
 function initListeners() {
-    $("nav a").on("click", (e) => {
-      let btnID = e.currentTarget.id;
-      loadContent(btnID);
-
-    });
-  }
-  
-  function loadContent(pageName) {
- 
-    $.get(`../pages/${pageName}.html`, (data) => {
-      $("#app").html(data);
-
-    }).fail((error) => {
- 
-      alert("Page is " + error.statusText);
-    });
- 
-  
-  }
-  
-  $(document).ready(function () {
-    loadContent("home");
-
-    initListeners();
+  $("nav a").on("click", (e) => {
+    let btnID = e.currentTarget.id;
+    loadContent(btnID);
   });
-  
+}
+
+function loadContent(pageName) {
+  $.get(`../pages/${pageName}.html`, (data) => {
+    $("#app").html(data);
+  }).fail((error) => {
+    alert("Page is " + error.statusText);
+  });
+}
+
+
+$(document).ready(function () {
+  loadContent("home");
+  initListeners();
+});
