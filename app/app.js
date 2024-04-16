e48c6cf0addc4b0501153f4065b8e46aab265433
arigree <arigree@iu.edu>
2024-04-16 05:07:54 -0400

    adding page info, styling, js, etc

---
+++ app/app.js
@@ -0,0 +1,27 @@
+function initListeners() {
+    $("nav a").on("click", (e) => {
+      let btnID = e.currentTarget.id;
+      loadContent(btnID);
+
+    });
+  }
+  
+  function loadContent(pageName) {
+ 
+    $.get(`../pages/${pageName}.html`, (data) => {
+      $("#app").html(data);
+
+    }).fail((error) => {
+ 
+      alert("Page is " + error.statusText);
+    });
+ 
+  
+  }
+  
+  $(document).ready(function () {
+    loadContent("home");
+
+    initListeners();
+  });
+  
